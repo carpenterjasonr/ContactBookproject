863025b3ff8c8c62fedb5f064520cc5b88eb5ce4
Jason Carpenter <48468892+carpenterjasonr@users.noreply.github.com>
2021-04-05 00:57:10 -0700

    Almost there

--- contactbook/model.py
+++ contactbook/model.py
@@ -20,4 +20,27 @@ class ContactsModel:
         headers = ("ID", "Name", "Job", "Email") #Setting user-readble headers for the table columns
         for columnIndex, header in enumerate(headers):
             tableModel.setHeaderData(columnIndex, Qt.Horizontal, header)
-        return tableModel 
+        return tableModel
+
+    def addContact(self, data):
+        # Add a contact to the database.
+        rows = self.model.rowCount()
+        self.model.insertRows(rows, 1)
+        for column, field in enumerate(data):
+            self.model.setData(self.model.index(rows, column + 1), field)
+        self.model.submitAll()
+        self.model.select()
+
+    def deleteContact(self, row):
+        # Remove a contact from the database.
+        self.model.removeRow(row)
+        self.model.submitAll()
+        self.model.select()
+
+    def clearContacts(self):
+        # Remove all contacts in the database.
+        self.model.setEditStrategy(QSqlTableModel.OnManualSubmit)
+        self.model.removeRows(0, self.model.rowCount())
+        self.model.submitAll()
+        self.model.setEditStrategy(QSqlTableModel.OnFieldChange)
+        self.model.select()
